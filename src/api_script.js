36f22fea1baa0fab5af2c19e991f08153b674b67
Facaller <ashwin.metal13@gmail.com>
2025-07-05 15:45:45 +0200

    Add captureInput method

--- src/api_script.js
+++ src/api_script.js
@@ -1,10 +1,14 @@
 export class apiScript {
     constructor () {
+        this.input = document.querySelector('#search').value;
+        this.img   = document.querySelector('img');
+
         this.fetchGif();
     }
 
     fetchGif () {
-        const img = document.querySelector('img');
+        const img = this.img;
+        
         fetch('https://api.giphy.com/v1/gifs/translate?api_key=TU3kl13JYySh97lnVdvPFpKTlQ4FZyU6&s=fish', {mode: 'cors'})
             .then(function(response) {
                 return response.json();
@@ -12,5 +16,18 @@ export class apiScript {
             .then(function(response) {
                 img.src = response.data.images.original.url;
         });
+    }
+
+    captureInput () {
+        const input = this.input;
+        const trimmed = input.trim();
+
+        if (!trimmed) {
+            throw new Error('Please enter a valid search term');
+        } 
+        
+        if (trimmed) {
+            return trimmed;
+        }
     }
 }
